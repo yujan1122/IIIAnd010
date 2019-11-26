46687513c6b4534dbf2f3a1799c76fefb0bb197a
Annie <https://github.com/yujan1122/practice1.git>
2019-11-26 13:26:56 +0800

    detail page parameter setting

--- app/src/main/java/tw/org/iii/iiiand10/DetailActivity.java
+++ app/src/main/java/tw/org/iii/iiiand10/DetailActivity.java
@@ -5,10 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.HashMap;
 
 public class DetailActivity extends AppCompatActivity {
+    private String id, name,addr, hostwords, feauture, latlng, picurl, tel, heart;
+    private ImageView img;
+    private TextView content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,20 @@ public class DetailActivity extends AppCompatActivity {
         HashMap<String,String> row =
                 (HashMap<String, String>)(intent.getSerializableExtra("data"));
         Log.v("brad",row.get("Name"));
+
+        id = row.get("ID");
+        name = row.get("Name");
+        addr = row.get("Address");
+        hostwords = row.get("HostWords");
+        tel = row.get("Tel");
+        latlng = row.get("Coordinate");
+        feauture = row.get("FoodFeature");
+        picurl = row.get("PicURL");
+        heart = row.get("Heart");
+
+        img = findViewById(R.id.detail_img);
+        content = findViewById(R.id.detailContent);
+
 
     }
 }
