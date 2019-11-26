2842890eabc8598a4cca8de3f8ceb529b4d7fdca
Annie <https://github.com/yujan1122/practice1.git>
2019-11-26 13:45:17 +0800

    fetchRemoteImage編寫

--- app/src/main/java/tw/org/iii/iiiand10/DetailActivity.java
+++ app/src/main/java/tw/org/iii/iiiand10/DetailActivity.java
@@ -3,10 +3,15 @@ package tw.org.iii.iiiand10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 
 import java.util.HashMap;
 
@@ -37,10 +42,25 @@ public class DetailActivity extends AppCompatActivity {
 
         img = findViewById(R.id.detail_img);
         content = findViewById(R.id.detailContent);
+
+        fetchRemoteImage();
     }
 
-    //抓遠端影像資料
+    //抓遠端影像資料; 抓資料是StringRequest
     private void fetchRemoteImage(){
+        ImageRequest request = new ImageRequest(picurl,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {//回覆在這
+                        img.setImageBitmap(response);
+                    }
+                }, 0, 0, Bitmap.Config.ARGB_8888,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
 
+                    }
+                });
+        MainApp.queue.add(request);//創物件後,提出要求
     }
 }
